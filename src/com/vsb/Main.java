324a76d4f32f0089ac3bd2e46641a6e9bf4ade02
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-09-12 13:51:19 +0200

    feat(spring): init
    references #9

--- src/com/vsb/Main.java
+++ src/com/vsb/Main.java
@@ -1,7 +1,13 @@
 package com.vsb;
 
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
 public class Main {
 
     public static void main(String[] args) {
+        SpringApplication app = new SpringApplication(Main.class);
+        app.run(args);
     }
 }
