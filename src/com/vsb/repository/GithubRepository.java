b907018dfa8606c97a3d19d959fe0f48204aed84
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-09-12 12:22:56 +0200

    feat(Github): repository init
    resolves #5

---
+++ src/com/vsb/repository/GithubRepository.java
@@ -0,0 +1,40 @@
+package com.vsb.repository;
+
+import com.vsb.client.github.GithubClient;
+import org.kohsuke.github.*;
+
+import java.io.IOException;
+import java.util.List;
+
+public class GithubRepository {
+    final GithubClient githubClient = new GithubClient();
+    final GHRepository repository;
+
+    public GithubRepository(final String name) throws IOException {
+        repository = githubClient.github.getRepository(name);
+    }
+
+    public String getLanguage() {
+        return repository.getLanguage();
+    }
+
+    public GHIssue getIssue(final int id) throws IOException {
+        return repository.getIssue(id);
+    }
+
+    public List<GHIssue> getIssues() throws IOException {
+        return repository.getIssues(GHIssueState.ALL);
+    }
+
+    public List<GHIssue> getIssues(final GHIssueState state) throws IOException {
+        return repository.getIssues(state);
+    }
+
+    public List<String> getTopics() throws IOException {
+        return repository.listTopics();
+    }
+
+    public PagedIterable<GHLabel> getLabels() throws IOException {
+        return repository.listLabels();
+    }
+}
