6b257c53011cd0d7f3a7894f65561bd2686c77af
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-09-13 20:59:01 +0200

    feat(api): :sparkles: issue controller

--- src/com/vsb/api/rest/IssueController.java
+++ src/com/vsb/api/rest/IssueController.java
@@ -1,31 +1,22 @@
 package api.rest;
 
-import org.kohsuke.github.GHIssue;
+import java.io.IOException;
+
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
 
+import dto.IssueEstimationProperties;
+import service.EstimationService;
+
 @CrossOrigin
 @RestController
+@RequestMapping("/issue")
 public class IssueController {
     
-    @RequestMapping(value = "/issues/{id}", method = RequestMethod.GET)
-    public ResponseEntity<GHIssue> getIssue(@PathVariable("id") String id) {
-        return new ResponseEntity<GHIssue>(new GHIssue(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/issues", method = RequestMethod.POST)
-    public ResponseEntity<GHIssue> createIssue(@RequestBody GHIssue issue) {
-        return new ResponseEntity<GHIssue>(new GHIssue(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/issues/{id}", method = RequestMethod.PUT)
-    public ResponseEntity<GHIssue> updateIssue(@PathVariable("id") String id, @RequestBody GHIssue issue) {
-        return new ResponseEntity<GHIssue>(new GHIssue(), HttpStatus.OK);
-    }
-
-    @RequestMapping(value = "/issues/{id}", method = RequestMethod.DELETE)
-    public ResponseEntity<GHIssue> deleteIssue(@PathVariable("id") String id) {
-        return new ResponseEntity<GHIssue>(new GHIssue(), HttpStatus.OK);
+    @RequestMapping(value = "/estimateProperties/{owner}/{repo}", method = RequestMethod.GET)
+    public ResponseEntity<IssueEstimationProperties> issuePropsEstimates(@PathVariable("owner") final String owner, @PathVariable("repo") final String repo) throws IOException {
+        var estimatedProperties = new EstimationService().estimateProperties(owner, repo);
+        return new ResponseEntity<IssueEstimationProperties>(estimatedProperties, HttpStatus.OK);
     }
 }
