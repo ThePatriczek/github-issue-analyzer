1369d9ea34242790091e2b4ca31b145a9c4c21e1
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-09-19 18:28:00 +0200

    feat(dto): user

---
+++ src/com/vsb/dto/User.java
@@ -0,0 +1,25 @@
+package dto;
+
+public class User {
+    final private String login;
+    final private String name;
+    final private String avatarUrl;
+
+    public User(String login, String name, String avatarUrl) {
+        this.name = name;
+        this.avatarUrl = avatarUrl;
+        this.login = login;
+    }
+
+    public String getLogin() {
+        return login;
+    }
+
+    public String getAvatarUrl() {
+        return avatarUrl;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
