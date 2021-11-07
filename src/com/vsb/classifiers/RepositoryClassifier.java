cb360e2514cb69d1a1207e3ded19509a7440c9c2
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-11-07 12:42:27 +0100

    refactor(repository): name

--- src/com/vsb/classifiers/RepositoryClassifier.java
+++ src/com/vsb/classifiers/RepositoryClassifier.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:c08136989465e9d010aeba7427ae2cae62dcd107f189cbdcba8cdedc1119d562
-size 5611
+oid sha256:20d06f5f15a6ef8d636de81a89ccb203effd1a84897d05b7a6b5b70e2e0f2356
+size 5643
