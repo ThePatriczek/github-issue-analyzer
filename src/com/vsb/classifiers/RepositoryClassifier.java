462800b4e91e22cc97b4a2925e2e8ef7a218dba4
Patrik Szewczyk <patrik.szewczyk@gmail.com>
2021-11-27 10:49:52 +0100

    refactor(repository): similar constants

--- src/com/vsb/classifiers/RepositoryClassifier.java
+++ src/com/vsb/classifiers/RepositoryClassifier.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:e977f5d8b6878697df6e83ba8245e2a8ac5441e52ccd43e424a4676579cc25bb
-size 5952
+oid sha256:143a7d5722e6ba844f51427a4747b8473943ad050e42001ad2a57bab5f710733
+size 5938
--- src/com/vsb/model/RepositoryVector.java
+++ src/com/vsb/model/RepositoryVector.java
@@ -1,3 +1,3 @@
 version https://git-lfs.github.com/spec/v1
-oid sha256:319abdafae76b9cd7d7b2e2ad80a7d447ba16af440a9eff574bbf1b107e0d851
-size 6119
+oid sha256:c858d907accda789698aa9929fc083a5e59caaaa33e223ae8c76714d818036d6
+size 6079
